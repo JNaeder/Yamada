a165813c95f3c50c1ac0be0c22001a7ae967991d
JNaeder <j.naeder324@gmail.com>
2019-04-11 14:31:58 -0400

    Init

---
+++ Testing 4-10-19/Assets/Enemy_1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_1 : MonoBehaviour
+{
+
+    public Transform startPos, endPos;
+    public float movementSpeed = 1;
+    public float buffer = 0.5f;
+
+    bool isGoingForward = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGoingForward)
+        {
+            GoToNextPosition(startPos.position);
+        }
+        else {
+            GoToNextPosition(endPos.position);
+        }
+
+        CheckPos();
+    }
+
+
+    void GoToNextPosition(Vector3 nextPos) {
+        Vector3 diff = nextPos - transform.position;
+        transform.position += diff * Time.deltaTime * movementSpeed;
+
+    }
+
+    void CheckPos() {
+        if (transform.position.x > endPos.position.x - buffer)
+        {
+            isGoingForward = true;
+            Debug.Log("Shit!");
+        }
+        else if (transform.position.x < startPos.position.x + buffer) {
+            isGoingForward = false;
+            Debug.Log("Fufkc");
+        }
+
+
+        Debug.Log(isGoingForward + " " + transform.position.x + ": start: " + startPos.position.x + " end: " + endPos.position.x);
+    }
+}
---
+++ Testing 4-10-19/Assets/Movement_1.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_1 : MonoBehaviour
+{
+    Rigidbody2D rB;
+    SpriteRenderer sP;
+    Animator anim;
+
+
+    public float speed = 2;
+    public float jumpForce;
+    public float fallMult = 1;
+    public float jumpDamp = 1;
+    public bool isGrounded = false;
+
+    public Transform groundCheck;
+    public LayerMask NonPlayerLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB = GetComponent<Rigidbody2D>();
+        sP = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Jumping();
+        CheckGround();
+    }
+
+
+    void Movement() {
+        float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
+        bool isWalking = Mathf.Abs(h) > 0.1f;
+        anim.SetBool("isWalking", isWalking);
+;
+
+        transform.position += new Vector3(h, 0, 0) * Time.deltaTime * speed;
+
+
+        if (h > 0)
+        {
+            sP.flipX = false;
+        }
+        else if (h < 0) {
+            sP.flipX = true;
+        }
+    }
+
+
+    void Jumping() {
+        if (Input.GetButtonDown("Jump")) {
+            rB.AddForce(new Vector2(0, 100) * jumpForce);
+        }
+
+
+        if (rB.velocity.y < 0) {
+            //make player fall faster
+            rB.velocity += Vector2.up * Physics2D.gravity.y * Time.deltaTime * fallMult;
+        } else if (rB.velocity.y > 0 && !Input.GetButton("Jump")) {
+            rB.velocity += Vector2.up * Physics2D.gravity.y * Time.deltaTime * jumpDamp;
+        }
+    }
+
+
+    void CheckGround() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.5f, NonPlayerLayer);
+        anim.SetBool("isGrounded", isGrounded);
+        Debug.Log(isGrounded);
+
+    }
+}
