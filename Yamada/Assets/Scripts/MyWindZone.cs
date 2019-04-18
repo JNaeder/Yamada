4fbb6dc7d12364c4fb4f2a7811124068bfed3298
JNaeder <J.naeder324@gmail.com>
2019-04-17 22:41:12 -0400

    Added Windzones

---
+++ Yamada/Assets/Scripts/MyWindZone.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyWindZone : MonoBehaviour
+{
+
+    Collider2D windZoneBox;
+    ParticleSystem[] pS;
+
+    float windZoneHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        windZoneBox = GetComponentInChildren<Collider2D>();
+        windZoneHeight = windZoneBox.bounds.size.y;
+
+
+        pS = GetComponentsInChildren<ParticleSystem>();
+
+
+        for(int i = 0; i < pS.Length; i++) {
+            pS[i].startLifetime = CalculatePSLifeTime(windZoneHeight);
+
+         
+           }
+
+
+    }
+
+
+    float CalculatePSLifeTime(float wZHeight) {
+        float pSLifeTime = windZoneHeight * 0.11f;
+
+
+        return pSLifeTime;
+       }
+
+
+}
