28d3a1578f01b3c12d698d7ec26a262ac4f4b569
JNaeder <J.naeder324@gmail.com>
2019-05-07 19:25:31 -0400

    dsdsds

---
+++ Yamada/Assets/Scripts/EndDiamond.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDiamond : MonoBehaviour
+{
+    GameManager gM;
+    public GameObject hitExplosion;
+
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponentInParent<Animator>();
+        gM = FindObjectOfType<GameManager>();
+    }
+
+
+    private void Update()
+    {
+
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("End Level!");
+            anim.Play("EndDiamond_Got");
+
+        }
+    }
+
+
+    public IEnumerator PlayParticleAndLoadScene()
+    {
+        Instantiate(hitExplosion, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+        gM.LoadScene(0);
+    }
+}
---
+++ Yamada/Assets/Scripts/MapTrigger.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTrigger : MonoBehaviour
+{
+
+    public GameObject interactKeyImage;
+    public GameObject mapMenuUI;
+
+
+    Movement_1 player;
+
+
+    private void Start()
+    {
+        player = FindObjectOfType<Movement_1>();
+    }
+
+    private void Update()
+    {
+        InputControl();
+    }
+
+
+    void InputControl()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            ShowMap();
+            interactKeyImage.SetActive(false);
+
+        }
+
+    }
+
+    void ShowMap()
+    {
+        Debug.Log("Show Map!");
+        mapMenuUI.SetActive(true);
+        player.isMoveable = false;
+    }  
+
+    public void TurnOffMap()
+    {
+        mapMenuUI.SetActive(false);
+        player.isMoveable = true;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interactKeyImage.SetActive(true);
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interactKeyImage.SetActive(false);
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Collider2D coll = GetComponent<Collider2D>();
+        Color newColor = Color.yellow;
+        Gizmos.DrawWireCube(transform.position + new Vector3(coll.offset.x, coll.offset.y, 0), new Vector2(coll.bounds.size.x + 1, coll.bounds.size.y));
+        newColor.a = 0.15f;
+        Gizmos.color = newColor;
+        Gizmos.DrawCube(transform.position + new Vector3(coll.offset.x, coll.offset.y, 0), new Vector2(coll.bounds.size.x + 1, coll.bounds.size.y));
+
+
+    }
+}
